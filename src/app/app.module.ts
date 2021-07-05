ab35f9cd08104c9df448ba0472f3b9b5744054bb
Nestor Matias <solomatias321@gmail.com>
2021-07-05 12:09:49 +0200

    creating the best structure and adding routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DefaultModule } from './layouts/default/default.module';
+import { SharedModule } from './shared/shared.module';
 @NgModule({
   declarations: [
     AppComponent
@@ -12,6 +14,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    DefaultModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
