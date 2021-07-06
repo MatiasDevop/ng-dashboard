28b7052ad9cd25c2aa78ad3f35e104d7c06374ca
Nestor Matias <nestormatias@MacBook-Pro.home>
2021-07-06 11:55:52 +0200

    adding highchart are

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  @Output() toogleSideBarForMe: EventEmitter<any> = new EventEmitter();
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  toogleSideBar(){
+    this.toogleSideBarForMe.emit();
+  }
 }
